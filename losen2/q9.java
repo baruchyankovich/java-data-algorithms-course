ec6ff54d0b0cfe4ea1ba9075359918ad587e7c71
Baruch Yankovitz <baruchyankovitz@gmail.com>
2025-01-28 14:25:15 +0200

    Add losen2/q9.java

---
+++ losen2/q9.java
@@ -0,0 +1,30 @@
+package losen2;
+import java.util.Scanner;
+
+public class q9 {
+
+    public static void main (String[] args){
+        Scanner scanner =new Scanner(System.in);
+        int num1 ,num2;
+        System.out.println("Enter 2 numbers with the same number if digits ");
+        System.out.println("Enter the first number: ");
+        num1=scanner.nextInt;
+        System.out.println("Enter the secend number: ");
+        num2=scanner.nextInt;
+
+    
+
+    }
+    public static int margeTheNunbers (int num1,int num2){
+        int result;
+        while () {
+            
+        }
+        
+        
+        
+        
+        return result;
+    }
+
+}
