7c560f44a4042bb2b252c9ca1021f8cc8748c9b7
Baruch Yankovitz <baruchyankovitz@gmail.com>
2025-01-28 19:39:48 +0200

    updata losen2/q9.java

--- losen2/q9.java
+++ losen2/q9.java
@@ -8,23 +8,38 @@ public class q9 {
         int num1 ,num2;
         System.out.println("Enter 2 numbers with the same number if digits ");
         System.out.println("Enter the first number: ");
-        num1=scanner.nextInt;
+        num1=scanner.nextInt();
         System.out.println("Enter the secend number: ");
-        num2=scanner.nextInt;
+        num2=scanner.nextInt();
+        System.out.println("the marge of : "+num1+" and: "+ num2 +" is " + buildNewNumber (num1,num2));
 
     
 
     }
-    public static int margeTheNunbers (int num1,int num2){
-        int result;
-        while () {
-            
+    public static int buildNewNumber(int num1, int num2) {
+        int result = 0;
+        int multiplier = 1;
+
+        while (num1 > 0 && num2 > 0) {
+            int digit1 = num1 % 10; 
+            int digit2 = num2 % 10; 
+
+            for (int i = 0; i < digit1; i++) {
+                result += digit2 * multiplier;
+                multiplier *= 10;
+
+                if (multiplier > 1_000_000_000) {
+                    System.out.println("The result exceeds 9 digits, truncating.");
+                    return result;
+                }
+            }
+
+            num1 /= 10;
+            num2 /= 10;
         }
-        
-        
-        
-        
+
         return result;
     }
-
 }
+
+
