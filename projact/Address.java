52def8f73374944aab5cad675b7d57264c4da258
Baruch Yankovitz <baruchyankovitz@gmail.com>
2025-02-13 12:30:55 +0200

    Added new folder: projact

---
+++ projact/Address.java
@@ -0,0 +1,16 @@
+package projact;
+
+public class Address {
+    private String street;
+    private int buildingNumber;
+    private String city;
+    private String country;
+
+    public Address(String street, int buildingNumber, String city, String country) {
+        this.street = street;
+        this.buildingNumber = buildingNumber;
+        this.city = city;
+        this.country = country;
+    }
+
+}
